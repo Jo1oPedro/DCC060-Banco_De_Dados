5480850494a46b3528d6ae4c9e1466699ed6169f
João Pedro <joao.pedreira@estudante.ufjf.br>
2023-01-10 01:12:15 -0300

    Adiciona autoload e instancia global de acesso ao banco

---
+++ src/Source/Database/Connect.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Ggj\BancoDeDados\Source\Database;
+
+use PDO;
+use PDOException;
+
+class Connect
+{
+    private const HOST = "localhost";
+    private const USER = "root";
+    private const DBNAME = "dcc060_banco_de_dados";
+    private const PASSWD = "";
+
+    private const OPTIONS = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ];
+
+    private static $instance;
+
+    /**
+     * @return PDO
+     */
+    public static function getInstance(): PDO
+    {
+        if(empty(self::$instance)) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:dbname=" . self::DBNAME . ";host=" . self::HOST . ":3306",
+                    self::USER,
+                    self::PASSWD,
+                    self::OPTIONS
+                );
+            } catch (PDOException $exception) {
+                die(var_dump($exception->getMessage()));
+                die("<h1>Ooopss! Erro ao conectar...</h1>");
+            }
+        }
+
+        return self::$instance;
+    }
+
+    final public function __construct()
+    {
+    }
+
+    final public function __clone(): void
+    {
+    }
+}
---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+//use Ggj\BancoDeDados\Source\Database\Connect;
+//$pdo = new Connect();
+print_r($pdo);
