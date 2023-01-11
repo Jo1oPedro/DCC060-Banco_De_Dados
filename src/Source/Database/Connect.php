4facffa51b0ba9c8458ea37a15854055e1bed613
Gabriel Frasson <bielfc27@gmail.com>
2023-01-10 23:29:50 -0300

    Postgres connection

--- src/Source/Database/Connect.php
+++ src/Source/Database/Connect.php
@@ -8,9 +8,10 @@ use PDOException;
 class Connect
 {
     private const HOST = "localhost";
-    private const USER = "root";
+    private const PORT = 5432;
+    private const USER = "";
     private const DBNAME = "dcc060_banco_de_dados";
-    private const PASSWD = ""; 
+    private const PASSWD = "";
 
     private const OPTIONS = [
         //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
@@ -29,7 +30,7 @@ class Connect
         if(empty(self::$instance)) {
             try {
                 self::$instance = new PDO(
-                    "pgsql:dbname=" . self::DBNAME . ";host=" . self::HOST . ":5432",
+                    "pgsql:dbname=" . self::DBNAME . ";host=" . self::HOST . ";port=" . self::PORT,
                     self::USER,
                     self::PASSWD,
                     self::OPTIONS
